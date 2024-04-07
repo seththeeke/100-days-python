21e97219a4e0c01c9190c6a4ad54616dfad7aa59
Seth Theeke <theekes@amazon.com>
2024-04-07 18:59:00 -0500

    Day 1 Band Name Generator

---
+++ Day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is your pet's name?\n")
+print("Your band name: " + city + " " + pet_name)
