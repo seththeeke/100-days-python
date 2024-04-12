621987d15e815fe8a59e5024751589e1c89b7f14
Seth Theeke <theekes@amazon.com>
2024-04-12 08:33:45 -0500

    Day 6 - Karel the Robot

---
+++ Day6/main.py
@@ -0,0 +1,31 @@
+"""
+Day 6 - Functions, Code Blocks, and While Loops
+
+Functions are declared via def keyword and then a name, parameters, and colon
+ - def my_function():
+While loops execute until the condition is false
+ - while condition:
+"""
+
+# Hurdles Leap Challenge - Assuming Karels Robot is available
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump_hurdle():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# move()
+# jump_hurdle()
+# move()
+# jump_hurdle()
+# ... and so on
+
+# Day 6 - Solve the maze
+# Did not write code because it was in Karel tool...
