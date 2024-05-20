95b2c43bddfd44d81c56f963678cfacd0c64b7d7
Seth Theeke <seththeeke@gmail.com>
2024-05-20 18:02:03 -0500

    Day 27 - Miles to Kilos Converter TKinter

--- Day27/main.py
+++ Day27/main.py
@@ -12,6 +12,8 @@ Advanced Arguments
 
 import tkinter as tk
 
+
+
 window = tk.Tk()
 window.title("Hello World")
 
@@ -19,10 +21,24 @@ my_label = tk.Label(text="My Label", font=("Helvetica", 20))
 # The pack function allows you to configure how the component is shown on screen
 my_label.pack()
 
+mile_input = tk.Entry(width=10)
+mile_input.pack()
+
+kilo_label = tk.Label(width=10)
+kilo_label.pack()
 
 
+def convert_to_kilos():
+    mile_value = float(mile_input.get())
+    kilo_value = mile_value * 1.609344
+    kilo_label["text"] = str(kilo_value)
+    kilo_label.pack()
+    pass
 
 
+button = tk.Button(text="Convert", command=convert_to_kilos)
+button["text"] = "Convert"
+button.pack()
 
 # Must end the program
 window.mainloop()
