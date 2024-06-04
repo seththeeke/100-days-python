9a9951ae1a96c7ebe819906206eac2344b6487dd
Seth Theeke <seththeeke@gmail.com>
2024-06-04 07:30:10 -0500

    Day 28 Part 2, Timer

--- Day28/main.py
+++ Day28/main.py
@@ -7,28 +7,33 @@ import tkinter as tk
 FIVE_MINUTES = 5 * 60
 TWENTY_FIVE_MINUTES = 25 * 60
 TWENTY_MINUTES = 20 * 60
-
-
+FIVE_SECONDS = 5
 
 def start():
-    count_down()
-    pass
+    count_down(FIVE_SECONDS)
 
 
 def reset():
     pass
 
 
-def count_down():
-    curr_timer -= 1
-    print(curr_timer)
-    window.after(1000, count_down)
+def get_string_time(timer):
+    minutes = int(timer / 60)
+    seconds = timer % 60
+    return f"{minutes}:{seconds}"
 
-start_timer = TWENTY_FIVE_MINUTES
-curr_timer = start_timer
+def count_down(timer):
+    if timer > 0:
+        timer -= 1
+        label.config(text=f"{get_string_time(timer)}")
+        window.after(1000, count_down, timer)
+
 
 window = tk.Tk()
 window.title("Pomodoro Technique")
+
+label = tk.Label(window, text="25:00")
+label.pack()
 
 button = tk.Button(text="Start", command=start)
 button.pack()
